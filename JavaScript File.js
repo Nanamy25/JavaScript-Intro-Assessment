5bcce42f17df6635fe0092847a1e8c8884be1173
Annabell <annabellrambla@gmail.com>
2023-09-08 21:34:17 -0400

    Added JavaScript Assignment into repository

---
+++ JavaScript File.js
@@ -0,0 +1,9 @@
+var firstName;
+var lastName;
+var age;
+firstName = " Annabell";
+lastName = " Rambla";
+age = 19;
+alert ("My name is" +firstName +lastName);
+alert("I am " +age +" years old");
+console.log ("I am having lots of fun learning in this class, makes me feel like I'm playing Doki Doki a little bit")
